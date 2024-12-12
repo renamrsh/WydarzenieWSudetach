948b91bbbb0b135fd4b69c51e16c7f0c93cf9776
renamrsh <irenamarsh06@gmail.com>
2024-12-12 16:11:17 +0100

    ff

--- app/src/main/java/com/example/wydarzeniewsudetach/MainActivity.java
+++ app/src/main/java/com/example/wydarzeniewsudetach/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.wydarzeniewsudetach;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView licznik, komunikat;
     private EditText email, haslo, powtorz;
     private Button polub, usun, zapisz,zobacz;
+    private int counter =0;
+    private boolean rejestracja = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,45 @@ public class MainActivity extends AppCompatActivity {
         usun = findViewById(R.id.usun);
         zapisz = findViewById(R.id.zapisz);
         zobacz = findViewById(R.id.zobacz);
-
+        komunikat.setText("Obszar komunikatów");
+        polub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                update(counter++);
+            }
+        });
+        usun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(counter>0){
+                    update(counter);
+                }
+            }
+        });
+        zapisz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rejestracja = false;
+                if(!(email.getText().toString().trim()).contains("@")){
+                   komunikat.setText("Nieprawidlowy adres e-mail");
+                } else if (!(haslo.getText().toString().trim()).equals(powtorz.getText().toString().trim())){
+                    komunikat.setText("Hasła się różną");
+                }else{
+                    komunikat.setText("Zarejestrowano "+ email.getText().toString().trim());
+                    rejestracja = true;
+                }
+            }
+        });
+        zobacz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(rejestracja){
+                    komunikat.setText(email.getText().toString().trim());
+                }
+            }
+        });
+    }
+    public void update(int counter){
+        licznik.setText(counter + " polubień");
     }
 }
